0de94744276b37c3207736738250ce1b3cdeea59
Spectre <nf-projects@users.noreply.github.com>
2022-04-05 09:25:55 -0700

    check for metamask

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React from "react";
-import { ethers } from "ethers";
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-export default function App() {
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
 
-  const wave = () => {
-    
+    if (!ethereum) {
+      // if it's not defined, the user doesn't have metamask
+      console.log("Make sure you have metamask!");
+      alert("Make sure you have metamask!");
+    } else {
+      // otherwise, the user does have metamask
+      console.log("We have the ethereum object", ethereum);
+      alert("We have the ethereum object");
+    }
   }
-  
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
 }
+
+export default App
