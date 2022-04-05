d26854ffc2d12c2f0a50f11cad9cbc933698e140
Spectre <nf-projects@users.noreply.github.com>
2022-04-05 09:33:33 -0700

    attempt to access metamask account

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
 
-    if (!ethereum) {
-      // if it's not defined, the user doesn't have metamask
-      console.log("Make sure you have metamask!");
-      alert("Make sure you have metamask!");
-    } else {
-      // otherwise, the user does have metamask
-      console.log("We have the ethereum object", ethereum);
-      alert("We have the ethereum object");
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+
+
+  const checkIfWalletIsConnected = () => {
+    try {
+      /*
+      * First make sure we have access to window.ethereum
+      */
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        // if it's not defined, the user doesn't have metamask
+        console.log("Make sure you have metamask!");
+        alert("Make sure you have metamask!");
+      } else {
+        // otherwise, the user does have metamask
+        console.log("We have the ethereum object", ethereum);
+        alert("We have the ethereum object");
+  
+        /*
+        * Check if we're authorized to access the user's wallet
+        */
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+  
+        if (accounts.length !== 0) {
+          // if the length is not 0, we have an account
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+        } else {
+          console.log("No authorized account found")
+          alert("No authorized MetaMask account found!")
+        }      
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
